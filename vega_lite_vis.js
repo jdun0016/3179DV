6d5da02f81cc1709e50cc2d4f6a160e203b6b83c
jdun0016 <115064135+jdun0016@users.noreply.github.com>
2022-10-17 12:56:03 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,18 @@ var vg_1 = "NetflixMap.json";
 vegaEmbed("#netflix_cost_map", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+var vg_2 = "NumMoviesBarChart.json";
+vegaEmbed("#movie_bar_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "bubble.json";
+vegaEmbed("#bubble_chart", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "LineChart.json";
+vegaEmbed("#line_chart", vg_4).then(function(result) {
+
+}).catch(console.error);
